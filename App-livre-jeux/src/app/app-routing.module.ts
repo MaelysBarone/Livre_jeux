9d057cc33f534605a097d21e8850a0c64eaa6ff7
Lsdla <al3s2@yahoo.fr>
2021-06-08 13:54:31 +0200

    integration bootstrap et création de la nav bar

--- App-livre-jeux/src/app/app-routing.module.ts
+++ App-livre-jeux/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfilComponent } from './authenticate/profil/profil.component';
+import { EditorLibraryComponent } from './components/editor/editor-library/editor-library.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LayoutComponent,
+     children: [
+      {path:'', redirectTo:'home', pathMatch:"full"},
+      {path:'home', component:HomeComponent},
+      {path:'profil', component:ProfilComponent},
+      {path:'editor-library', component:EditorLibraryComponent},
+      {path:'not-found', component:ErrorPageComponent},
+      {path:'**', redirectTo:'/not-found'},
+     ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
